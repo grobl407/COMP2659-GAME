b5ece7d0f392fba0b9d5fc370f5b16cc5d628195
IcedMatchaMilkT <145057660+IcedMatchaMilkT@users.noreply.github.com>
2025-02-23 16:19:35 -0700

    Update event.c

--- event.c
+++ event.c
@@ -1,11 +1,6 @@
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 400
 
-void move_ball (Ball *ball) {
-  ball->x += ball->delta_x;
-  ball->y += ball->delta_y;
-}
-
 void move_paddle (Paddle *paddle, int direction) {
   if (direction == 0) { /* moving left */
     if (paddle->x > 0)
