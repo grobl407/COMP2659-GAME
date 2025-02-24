81bfd9a3b54f1dbb46c0aeb072e84cb2bba81724
IcedMatchaMilkT <145057660+IcedMatchaMilkT@users.noreply.github.com>
2025-02-24 11:58:08 -0700

    Update event.c

--- Stage 3/event.c
+++ Stage 3/event.c
@@ -2,16 +2,42 @@
 #define SCREEN_HEIGHT 400
 
 void ball_falls () { /* Minus one life, ball restarts at rest */
+    if (ball->y >= floor->y) {
+    game->lives -= 1;
+    if (game->lives == 0) {
+      game->game_over = 1; // Game over is true
+    }
+    else {
+      reset_ball(ball, paddle);
+    }
+  }
 }
 
-void level_complete () { /* All bricks cleared, so replace bricks, award points,
-                          increase level and ball velocity, restart the ball */
+void reset_bricks (brick bricks[], int num_bricks, int level) {
+  for (int i = 0; i < num_bricks; i++) {
+    bricks[i].isBroken = 0;
+    bricks[i].n_hits = 0;
+    bricks[i].base_points = bricks[i].basepoints*2; // double points awarded for each broken brick at new level
+  }
+}
+
+/* All bricks cleared, so replace bricks, award points,
+increase level and ball velocity, restart the ball */
+void level_complete (game *game, ball *ball, brick bricks[], int num_bricks) {
+  /* First Check if all bricks are cleared */
+  int levelClear = 0; // Level clear starts true
+
+  for (int i = 0; i < num_bricks; i++) { // iterate through every brick
+    if (bricks[i].isBroken == 0) { // If brick is not broken, set levelClear to false,
+      levelClear = 0;
+    }
+  }
+
+  if (levelClear == 1) { // If level clear is true
+    game->level += 1; // Increment level
+    reset_bricks(bricks, num_bricks, game->level);
+  }
 }
 
 void game_over () { /* Ball falls and no lives remain */
 }
-
-void brick_breaks () { /* Clear brick and award points */
-}
-
-
