78ebed60e238762d70a07c854bd5525cc37b4d80
IcedMatchaMilkT <145057660+IcedMatchaMilkT@users.noreply.github.com>
2025-02-24 12:26:14 -0700

    Update model.c

--- Stage 3/model.c
+++ Stage 3/model.c
@@ -2,14 +2,14 @@
 #define SCREEN_HEIGHT 400
 #include "model.h"
 
-void move_ball (Ball *ball) {
+void move_ball (ball *ball) {
   if (ball->isActive = 1) {
     ball->x += ball->delta_x;
     ball->y += ball->delta_y;
   }
 }
 
-void move_paddle (Paddle *paddle, int direction) {
+void move_paddle (paddle *paddle, int direction) {
   if (direction == 0) { /* moving left */
     if (paddle->x > 0)
       paddle->x -= paddle->move_dist;
