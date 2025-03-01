67c501ea242961ac786329b174985c470a320817
IcedMatchaMilkT <145057660+IcedMatchaMilkT@users.noreply.github.com>
2025-02-28 21:47:56 -0700

    Update test_s3.c

--- test_s3.c
+++ test_s3.c
@@ -128,38 +128,100 @@ void bitmap_test_function() {
 	plot_paddle(base, 250, 350, platform_map);
 	Cnecin();
 }
+
+void print_ball_state (ball *ball) {
+  printf("Ball X: %d, Y: %d, delta_x: %d, delta_y: %d,
+    ball->x, ball->y, ball->delta_x, ball->delta_y);
+}
+
+void print_paddle_state (paddle *paddle) {
+  printf("Paddle left: %d, right: %d, Y: %d\n",
+    paddle->x, paddle->x + paddle_size_x, paddle->y);
+}
+
+void print_brick_state (int index, brick *brick) {
+  printf("Brick collision index: %d, x: %d, y: %d, broken: %d\n",
+    index, brick->x, brick->y, brick->isBroken);
+}
+
+void print_life_lost (game *game) {
+  printf("Life lost, %d lives remaining. \n",
+    game->lives);
+}
+
 int main() {
 /* initialize game elements */
 
-  wall left_wall;
-  left_wall->x = 160;
+  wall left_wall = {160};
+  wall right_wall = {480};
+  ceiling celing = {0};
+  floor floor = {400};
 
-  wall right_wall;
-  right_wall->x = 480;
+  ball ball = {320, 307, 2, 2, 1);
 
-  ball ball;
-  ball->ball_x = 320;
-  ball->ball_y = 307;
-  ball->delta_x = 2;
-  ball->delta_y = 2;
-  ball->isActive = 1;
-
-  paddle paddle;
-  paddle->x = 288;
-  paddle->y = 300;
-  paddle->move_dist = 10;
+  paddle paddle = {280, 300, 64, 8, 0, 10};
 
   brick bricks[40];
 
-  game game;
-  game->score = 0;
-  game->lives = 3;
-  game->level = 1;
-  game->game_over = 0;
+  game game = {0, 3, 1, 0, 0};
 
   int bricks_per_row = 8;
   int num_bricks = 40; // 5 rows 8 bricks
-
+  int brick_width = 32;
+  int brick_height = 7;
+  int spacing = 7;
+  int start_x = 171;
+  int start_y;
+  
   for (int i = 0; i < num_bricks; i++) {
-    brick *brick = &bricks[i];
-    brick->x = 171 + (i % bricks_per_row) * 
+    int row = i / bricks_per_row;
+    int column = i % bricks_per_row;
+
+    bricks[i].x = start_x + column * (brick_width + spacing);
+    bricks[i].y = start_y + row * (brick_height + spacing);
+    bricks[i].isBroken = 0;
+    bricks[i].base_points = 10;
+    bricks[i].size_x = brick_width;
+    bricks[i].size_y = brick_height;
+    bricks[i].health = 5;
+  }
+
+  char key;
+  while (1) {
+    if (_khbit()) {
+      key = _getch();
+
+      if (key == 'a') { // Move paddle left
+        move_paddle(&paddle, 0);
+        print_paddle_state(&paddle);
+      }
+      else if (key == 'd') { // Move paddle right
+        move_paddle(&paddle, 1);
+        print_paddle_state(&paddle);
+      }
+      else if (key == ' ') { // Space simulates one tick
+        move_ball(&ball);
+
+        int initial_x = ball.x;
+        int initial_y = ball.y;
+        ball_collisions(&ball, &paddle, bricks, num_bricks, &left_wall, &right_wall, &ceiling, &floor, &game);
+
+        if (ball.x != initial_x || ball.y != initial_y) { // Print ball state if it has moved
+          print_ball_state(&ball);
+        }
+
+        // Check which brick was hit
+        find_brick(&ball, bricks, num_bricks, &game);
+
+        if (game.lives == 0) {
+          game.game_over = 1;
+        }
+      }
+      else if (key == 'q') {
+        return 0;
+      }
+    }
+  }
+  return 0;
+}
+
