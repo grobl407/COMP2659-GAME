4cdab6a392db739eb7c5fe2461eb6ccb4292ec3f
grobl407 <grobl407@mtroyal.ca>
2025-03-23 18:11:42 -0600

    started stage 4 rendering

--- Stage 4/render.c
+++ Stage 4/render.c
@@ -4,7 +4,7 @@
 #include "render.h"
 #include "TYPES.H"
 
-
+const int HEART_Y = 45;
 
 
 
@@ -12,13 +12,15 @@ void render(const Model *model, UINT8 *base){
 
 }
 void render_ball(const Ball *ball, UINT8 *base){
-
+    plot_ball(base, ball->x, ball->y, ball->ball_bitmap);
 }
 void render_brick(const Brick *brick, UINT8 *base){
 
 }
 void render_heart(const Heart *heart, UINT8 *base){
-
+    for(heart->x; heart->x < 219; heart->x += 24){
+        plot_heart(base, heart->x, HEART_Y, heart->heart_map);
+    }
 }
 void render_platform(const Platform *platform, UINT8 *base){
 
