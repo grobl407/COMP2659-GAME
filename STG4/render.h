da8fd0b7ee52b0d4c613c170a6ee1084cc04ef62
grobl407 <grobl407@mtroyal.ca>
2025-03-23 23:07:23 -0600

    working!

--- STG4/render.h
+++ STG4/render.h
@@ -3,16 +3,15 @@
 
 #include "objects.h"
 #include "types.h"
-#include "..\stg2\raster.h"
 #include <stdlib.h> 
 #include <stdio.h>
 
-/*const in the functions means it cannot modify the bitmaps (position, bitmap pointer) only draw it*/
+
 
 void render(Model *model, UINT8 *base); 
 void render_ball(Ball *ball, UINT8 *base);
 void render_brick(Brick *brick, UINT8 *base);
 void render_heart(Heart *heart, UINT8 *base);
 void render_platform(Platform *platform, UINT8 *base);
-
+void render_walls (UINT8 *base);
 #endif
