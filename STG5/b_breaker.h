ea708d754e9922fe0f4763fca34b0bacbaf41a32
IcedMatchaMilkT <145057660+IcedMatchaMilkT@users.noreply.github.com>
2025-03-24 21:16:39 -0600

    Update b_breaker.h

--- STG5/b_breaker.h
+++ STG5/b_breaker.h
@@ -4,8 +4,8 @@
 void initialize_game(Game *game, Ball *ball, Paddle *paddle, Brick bricks[], int num_bricks, Wall *left_wall, Wall *right_wall, Ceiling *ceiling, Floor *floor);
 void initialize_bricks(Brick *bricks[], int num_bricks);
 void process_input(Paddle *paddle, Ball *ball);
-void int main();
-void void render_brick(Brick *brick, UINT8 *base);
+int main();
+void render_brick(Brick *brick, UINT8 *base);
 void render_all_bricks(Brick bricks[], int num_bricks, UINT8 *base);
 
 #endif B_BREAKER_H
